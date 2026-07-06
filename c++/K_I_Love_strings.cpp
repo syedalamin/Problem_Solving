000882092369ad08f5e57185f1f074b190cd6627
syedalamine <syedalamin2580@gmail.com>
2026-07-07 04:50:04 +0600

    reverse and love string prblem solve

--- c++/K_I_Love_strings.cpp
+++ c++/K_I_Love_strings.cpp
@@ -3,36 +3,40 @@ using namespace std;
 
 int main()
 {
-    string s, t;
+    int n;
+    cin >> n;
 
-    cin >> s >> t;
-
-    // cout << s[0] << t[0] << s[1] << t[1];
-    // cout;
-
-    int len_s = s.size();
-    int len_t = t.size();
-
-    int i;
-    for (i = 0; i < len_s && len_t; i++)
+    while (n--)
     {
-        cout << s[i] << t[i];
-    }
 
-    if (i < len_s)
-    {
-        for (; i < len_s; i++)
+        string s, t;
+
+        cin >> s >> t;
+
+        int len_s = s.size();
+        int len_t = t.size();
+
+        int i;
+        for (i = 0; i < len_s && i < len_t; i++)    
         {
-            cout << s[i];
+            cout << s[i] << t[i];
         }
-    }
-    if ( i < len_t)
-    {
-        for (; i < len_t; i++)
-        {
-            cout << t[i];
-        }
-    }
 
+        if (i < len_s)
+        {
+            for (; i < len_s; i++)
+            {
+                cout << s[i];
+            }
+        }
+        if (i < len_t)
+        {
+            for (; i < len_t; i++)
+            {
+                cout << t[i];
+            }
+        }
+        cout << "\n";
+    }
     return 0;
 }
