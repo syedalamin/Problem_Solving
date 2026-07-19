ed86d947fd4314efc5ad6e806ddf0548c207918f
syedalamine <syedalamin2580@gmail.com>
2026-07-19 18:25:39 +0600

    link list mximum

---
+++ SinglyLinkedListMaximumValue.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = tail->next;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int maximum(Node *head)
+{
+    Node *temp = head;
+    int mx = INT_MIN;
+
+    for (Node *i = temp; i != NULL; i = i->next)
+    {
+        int val = i->val;
+
+        if (mx < val)
+        {
+
+            mx = val;
+        }
+    }
+    return mx;
+}
+
+int main()
+{
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+
+        insert_at_tail(head, tail, val);
+    }
+
+    maximum(head);
+
+    int mx = maximum(head);
+    cout << mx << "\n";
+
+    return 0;
+}
