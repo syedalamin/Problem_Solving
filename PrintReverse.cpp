97a590c24b1739c63e4c011ad0c8127daee9e020
syedalamine <syedalamin2580@gmail.com>
2026-07-25 13:57:43 +0000

    reverse

---
+++ PrintReverse.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+
+    tail = newNode;
+}
+
+void print_backward(Node *tail)
+{
+    Node *temp = tail;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+
+    while (true)
+    {
+        cin >> val;
+
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    print_backward(tail);
+
+    return 0;
+}
