e116a0fc4fa2939114166e7cd87405e51094b34a
syedalamine <syedalamin2580@gmail.com>
2026-08-10 18:20:39 +0000

    pos order

---
+++ BinaryTreeInputPosOrderOutput.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root = new Node(val);
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *newLeft;
+        Node *newRight;
+
+        if (l == -1)
+        {
+            newLeft = NULL;
+        }
+        else
+        {
+            newLeft = new Node(l);
+        }
+
+        if (r == -1)
+        {
+            newRight = NULL;
+        }
+        else
+        {
+            newRight = new Node(r);
+        }
+
+        p->left = newLeft;
+        p->right = newRight;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+void posOrder(Node * root){
+    if(root == NULL){
+        return;
+    }
+
+    posOrder(root->left);
+    posOrder(root->right);
+    cout<< root->val << " ";
+}
+
+int main()
+{
+    Node* root = inputTree();
+
+    posOrder(root);
+
+    return 0;
+}
