f1d69f32037ecb17d14739032571b41ccf6ac9a9
syedalamine <syedalamin2580@gmail.com>
2026-08-10 15:49:34 +0000

    pre order

---
+++ BinaryTreeInputPreOrderOutput.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root = new Node(val);
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *newLeft;
+        Node *newRight;
+
+        if (l == -1)
+        {
+            newLeft = NULL;
+        }
+        else
+        {
+            newLeft = new Node(l);
+        }
+        if (r == -1)
+        {
+            newRight = NULL;
+        }
+        else
+        {
+            newRight = new Node(r);
+        }
+
+        p->left = newLeft;
+        p->right = newRight;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+void preOrder(Node *root)
+{
+ 
+    if(root == NULL)
+    {
+        return;
+    }
+
+    cout << root->val << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main()
+{
+    Node *root = inputTree();
+
+    preOrder(root);
+    return 0;
+}
