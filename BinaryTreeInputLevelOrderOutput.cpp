5e4a39277189fa44f9f4fee5ec673137b9be7f2a
syedalamine <syedalamin2580@gmail.com>
2026-08-10 18:46:38 +0000

    level order

---
+++ BinaryTreeInputLevelOrderOutput.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root = new Node(val);
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *newLeft;
+        Node *newRight;
+
+        if (l == -1)
+        {
+            newLeft = NULL;
+        }
+        else
+        {
+            newLeft = new Node(l);
+        }
+
+        if (r == -1)
+        {
+            newRight = NULL;
+        }
+        else
+        {
+            newRight = new Node(r);
+        }
+
+        p->left = newLeft;
+        p->right = newRight;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        cout << p->val << " ";
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = inputTree();
+
+    levelOrder(root);
+
+    return 0;
+}
